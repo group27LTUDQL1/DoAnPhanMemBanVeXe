7260c3ca7d09a7c4a156b261be478302913b83e3
huynhbanghi1998 <33548906+huynhbanghi1998@users.noreply.github.com>
2018-11-15 12:18:28 +0700

    Tạo xong các form như đã thiết kế (chỉ có form trắng)

--- DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe/Form_Xe_7_Cho.cs
+++ DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe/Form_Xe_7_Cho.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace DoAnPhanMemBanVeXe
 {
-    public partial class Form1 : Form
+    public partial class Form_Xe_7_Cho : Form
     {
-        public Form1()
+        public Form_Xe_7_Cho()
         {
             InitializeComponent();
         }
