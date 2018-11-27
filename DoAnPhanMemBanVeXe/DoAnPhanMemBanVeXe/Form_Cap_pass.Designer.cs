445d2443790e4dbae4b607217a08a223f3b282e2
huynhbanghi1998 <33548906+huynhbanghi1998@users.noreply.github.com>
2018-11-28 01:08:13 +0700

    test

--- DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe/Form_Cap_pass.Designer.cs
+++ DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe/Form_Cap_pass.Designer.cs
@@ -28,25 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(109, 54);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // Form_add_Account_for_users
+            // Form_Cap_pass
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Name = "Form_add_Account_for_users";
+            this.Name = "Form_Cap_pass";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -54,8 +43,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
