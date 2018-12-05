c5b390885fb9bc73ce9e3f0f3fadb9c48855b4eb
huynhbanghi1998 <33548906+huynhbanghi1998@users.noreply.github.com>
2018-12-05 11:49:28 +0700

    đã làm xong giao diện 1 form

---
+++ DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe_2/Form/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnPhanMemBanVeXe_2
+{
+    public partial class Form_Cap_pass : DevComponents.DotNetBar.Office2007RibbonForm
+    {
+        public Form_Cap_pass()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Cap_pass_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Timer_Doi_Anh_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// xong button thoát
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            Timer_Doi_Anh.Stop();
+            this.Close();
+        }
+    }
+}
