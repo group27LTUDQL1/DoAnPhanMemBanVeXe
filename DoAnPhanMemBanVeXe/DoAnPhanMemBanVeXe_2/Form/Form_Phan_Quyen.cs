ea22eb7749bc5c0123dc6d28c9506eaa0f305980
NGHĨA LÊ THANH <1660371@student.hcmus.edu.vn>
2018-12-05 20:14:25 +0700

    thêm form Phan_Quyen

---
+++ DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe_2/Form/Form_Phan_Quyen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnPhanMemBanVeXe_2
+{
+    public partial class Form_Phan_Quyen : Form
+    {
+        public Form_Phan_Quyen()
+        {
+            InitializeComponent();
+        }
+    }
+}
