e8bd4e4b11d76251d0c60c73d426f678d770d859
Minh Nguyen <nguyenminh22051998@gmail.com>
2018-12-06 15:35:12 +0700

    Đã tạo xong các class cần thiết (các class đều trống)

---
+++ DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe_2/Ban_ve.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoAnPhanMemBanVeXe_2
+{
+    class Ban_ve
+    {
+    }
+}
