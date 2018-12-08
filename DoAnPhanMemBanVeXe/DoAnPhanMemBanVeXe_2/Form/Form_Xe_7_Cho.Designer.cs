f04eec9d8502ae07e282262d3182a194ec39df69
NGHĨA LÊ THANH <1660371@student.hcmus.edu.vn>
2018-12-08 14:17:21 +0700

    xong sự kiện cho form Form_Xe_7_Cho(chưa debug)

--- DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe_2/Form/Form_Xe_7_Cho.Designer.cs
+++ DoAnPhanMemBanVeXe/DoAnPhanMemBanVeXe_2/Form/Form_Xe_7_Cho.Designer.cs
@@ -40,9 +40,9 @@
             this.btn_6 = new DevComponents.DotNetBar.ButtonX();
             this.treeGX1 = new DevComponents.Tree.TreeGX();
             this.node1 = new DevComponents.Tree.Node();
-            this.nodeConnector1 = new DevComponents.Tree.NodeConnector();
             this.nodeConnector2 = new DevComponents.Tree.NodeConnector();
             this.elementStyle1 = new DevComponents.Tree.ElementStyle();
+            this.nodeConnector1 = new DevComponents.Tree.NodeConnector();
             this.PanelEx_Xe16.SuspendLayout();
             this.grb_7.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.treeGX1)).BeginInit();
@@ -57,9 +57,8 @@
             this.PanelEx_Xe16.DisabledBackColor = System.Drawing.Color.Empty;
             this.PanelEx_Xe16.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PanelEx_Xe16.Location = new System.Drawing.Point(0, 0);
-            this.PanelEx_Xe16.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.PanelEx_Xe16.Name = "PanelEx_Xe16";
-            this.PanelEx_Xe16.Size = new System.Drawing.Size(734, 668);
+            this.PanelEx_Xe16.Size = new System.Drawing.Size(489, 434);
             this.PanelEx_Xe16.Style.Alignment = System.Drawing.StringAlignment.Center;
             this.PanelEx_Xe16.Style.BackColor1.Color = System.Drawing.Color.DeepSkyBlue;
             this.PanelEx_Xe16.Style.BackColor2.Color = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
@@ -76,13 +75,13 @@
             this.btn_Thoat.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
             this.btn_Thoat.ColorTable = DevComponents.DotNetBar.eButtonColor.BlueOrb;
             this.btn_Thoat.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Thoat.Location = new System.Drawing.Point(514, 34);
-            this.btn_Thoat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Thoat.Location = new System.Drawing.Point(343, 22);
             this.btn_Thoat.Name = "btn_Thoat";
-            this.btn_Thoat.Size = new System.Drawing.Size(194, 88);
+            this.btn_Thoat.Size = new System.Drawing.Size(129, 57);
             this.btn_Thoat.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_Thoat.TabIndex = 2;
             this.btn_Thoat.Text = "Thoát";
+            this.btn_Thoat.Click += new System.EventHandler(this.btn_Thoat_Click);
             // 
             // grb_7
             // 
@@ -97,10 +96,9 @@
             this.grb_7.Controls.Add(this.btn_6);
             this.grb_7.DisabledBackColor = System.Drawing.Color.Empty;
             this.grb_7.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grb_7.Location = new System.Drawing.Point(18, 18);
-            this.grb_7.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.grb_7.Location = new System.Drawing.Point(12, 12);
             this.grb_7.Name = "grb_7";
-            this.grb_7.Size = new System.Drawing.Size(478, 622);
+            this.grb_7.Size = new System.Drawing.Size(319, 404);
             // 
             // 
             // 
@@ -138,13 +136,13 @@
             this.btn_2.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground;
             this.btn_2.ImageFixedSize = new System.Drawing.Size(60, 60);
             this.btn_2.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_2.Location = new System.Drawing.Point(243, 226);
-            this.btn_2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_2.Location = new System.Drawing.Point(162, 147);
             this.btn_2.Name = "btn_2";
-            this.btn_2.Size = new System.Drawing.Size(170, 138);
+            this.btn_2.Size = new System.Drawing.Size(113, 90);
             this.btn_2.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_2.TabIndex = 10;
             this.btn_2.Text = "2";
+            this.btn_2.Click += new System.EventHandler(this.btn_2_Click);
             // 
             // btn_TaiXe
             // 
@@ -153,13 +151,13 @@
             this.btn_TaiXe.Image = global::DoAnPhanMemBanVeXe_2.Properties.Resources.login;
             this.btn_TaiXe.ImageFixedSize = new System.Drawing.Size(50, 50);
             this.btn_TaiXe.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_TaiXe.Location = new System.Drawing.Point(48, 34);
-            this.btn_TaiXe.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_TaiXe.Location = new System.Drawing.Point(32, 22);
             this.btn_TaiXe.Name = "btn_TaiXe";
-            this.btn_TaiXe.Size = new System.Drawing.Size(160, 138);
+            this.btn_TaiXe.Size = new System.Drawing.Size(107, 90);
             this.btn_TaiXe.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_TaiXe.TabIndex = 13;
             this.btn_TaiXe.Text = "Tài xế";
+            this.btn_TaiXe.Click += new System.EventHandler(this.btn_TaiXe_Click);
             // 
             // btn_1
             // 
@@ -167,13 +165,13 @@
             this.btn_1.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground;
             this.btn_1.ImageFixedSize = new System.Drawing.Size(60, 60);
             this.btn_1.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_1.Location = new System.Drawing.Point(243, 34);
-            this.btn_1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_1.Location = new System.Drawing.Point(162, 22);
             this.btn_1.Name = "btn_1";
-            this.btn_1.Size = new System.Drawing.Size(170, 138);
+            this.btn_1.Size = new System.Drawing.Size(113, 90);
             this.btn_1.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_1.TabIndex = 4;
             this.btn_1.Text = "1";
+            this.btn_1.Click += new System.EventHandler(this.btn_1_Click);
             // 
             // btn_5
             // 
@@ -181,13 +179,13 @@
             this.btn_5.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground;
             this.btn_5.ImageFixedSize = new System.Drawing.Size(50, 50);
             this.btn_5.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_5.Location = new System.Drawing.Point(164, 400);
-            this.btn_5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_5.Location = new System.Drawing.Point(109, 260);
             this.btn_5.Name = "btn_5";
-            this.btn_5.Size = new System.Drawing.Size(135, 138);
+            this.btn_5.Size = new System.Drawing.Size(90, 90);
             this.btn_5.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_5.TabIndex = 2;
             this.btn_5.Text = "5";
+            this.btn_5.Click += new System.EventHandler(this.btn_5_Click);
             // 
             // btn_4
             // 
@@ -195,13 +193,13 @@
             this.btn_4.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground;
             this.btn_4.ImageFixedSize = new System.Drawing.Size(50, 50);
             this.btn_4.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_4.Location = new System.Drawing.Point(22, 400);
-            this.btn_4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_4.Location = new System.Drawing.Point(15, 260);
             this.btn_4.Name = "btn_4";
-            this.btn_4.Size = new System.Drawing.Size(135, 138);
+            this.btn_4.Size = new System.Drawing.Size(90, 90);
             this.btn_4.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_4.TabIndex = 7;
             this.btn_4.Text = "4";
+            this.btn_4.Click += new System.EventHandler(this.btn_4_Click);
             // 
             // btn_3
             // 
@@ -209,13 +207,13 @@
             this.btn_3.ColorTable = DevComponents.DotNetBar.eButtonColor.Office2007WithBackground;
             this.btn_3.ImageFixedSize = new System.Drawing.Size(60, 60);
             this.btn_3.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_3.Location = new System.Drawing.Point(48, 226);
-            this.btn_3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_3.Location = new System.Drawing.Point(32, 147);
             this.btn_3.Name = "btn_3";
-            this.btn_3.Size = new System.Drawing.Size(160, 138);
+            this.btn_3.Size = new System.Drawing.Size(107, 90);
             this.btn_3.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_3.TabIndex = 5;
             this.btn_3.Text = "3";
+            this.btn_3.Click += new System.EventHandler(this.btn_3_Click);
             // 
             // btn_6
             // 
@@ -224,24 +222,25 @@
             this.btn_6.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btn_6.ImageFixedSize = new System.Drawing.Size(50, 50);
             this.btn_6.ImagePosition = DevComponents.DotNetBar.eImagePosition.Top;
-            this.btn_6.Location = new System.Drawing.Point(308, 400);
-            this.btn_6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_6.Location = new System.Drawing.Point(205, 260);
             this.btn_6.Name = "btn_6";
-            this.btn_6.Size = new System.Drawing.Size(135, 138);
+            this.btn_6.Size = new System.Drawing.Size(90, 90);
             this.btn_6.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
             this.btn_6.TabIndex = 6;
             this.btn_6.Text = "6";
+            this.btn_6.Click += new System.EventHandler(this.btn_6_Click);
             // 
             // treeGX1
             // 
             this.treeGX1.AllowDrop = true;
             this.treeGX1.AutoScroll = true;
-            this.treeGX1.AutoScrollMinSize = new System.Drawing.Size(61, 35);
+            this.treeGX1.AutoScrollMinSize = new System.Drawing.Size(46, 29);
             this.treeGX1.CommandBackColorGradientAngle = 90;
             this.treeGX1.CommandMouseOverBackColor2SchemePart = DevComponents.Tree.eColorSchemePart.ItemHotBackground2;
             this.treeGX1.CommandMouseOverBackColorGradientAngle = 90;
             this.treeGX1.ExpandLineColorSchemePart = DevComponents.Tree.eColorSchemePart.BarDockedBorder;
-            this.treeGX1.Location = new System.Drawing.Point(302, 0);
+            this.treeGX1.Location = new System.Drawing.Point(201, 0);
+            this.treeGX1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.treeGX1.Name = "treeGX1";
             this.treeGX1.Nodes.AddRange(new DevComponents.Tree.Node[] {
             this.node1});
@@ -249,22 +248,17 @@
             this.treeGX1.NodeStyle = this.elementStyle1;
             this.treeGX1.PathSeparator = ";";
             this.treeGX1.RootConnector = this.nodeConnector1;
-            this.treeGX1.Size = new System.Drawing.Size(75, 23);
+            this.treeGX1.Size = new System.Drawing.Size(50, 15);
             this.treeGX1.Styles.Add(this.elementStyle1);
             this.treeGX1.SuspendPaint = false;
             this.treeGX1.TabIndex = 10;
             this.treeGX1.Text = "treeGX1";
-            this.treeGX1.Click += new System.EventHandler(this.treeGX1_Click);
             // 
             // node1
             // 
             this.node1.Expanded = true;
             this.node1.Name = "node1";
             this.node1.Text = "node1";
-            // 
-            // nodeConnector1
-            // 
-            this.nodeConnector1.LineWidth = 5;
             // 
             // nodeConnector2
             // 
@@ -294,14 +288,17 @@
             this.elementStyle1.PaddingTop = 3;
             this.elementStyle1.TextColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(128)))));
             // 
+            // nodeConnector1
+            // 
+            this.nodeConnector1.LineWidth = 5;
+            // 
             // Form_Xe_7_Cho
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(734, 668);
+            this.ClientSize = new System.Drawing.Size(489, 434);
             this.Controls.Add(this.treeGX1);
             this.Controls.Add(this.PanelEx_Xe16);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form_Xe_7_Cho";
             this.Text = "Form_Xe_7_Cho";
             this.Load += new System.EventHandler(this.Form_Xe_7_Cho_Load);
